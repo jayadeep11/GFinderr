ab1bfd989f4248c7358b4ac6011c256b414f3929
uyyalanaveen <21131a1254@gvpce.ac.in>
2024-05-18 13:03:52 +0530

    search-div

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import Search from "./components/Search";
+
 export default function App() {
   return (
+    <>
     <h1 className="text-[10rem] text-center font-bold ">
       GFinder
     </h1>
+    <Search />
+    </>
+    
   )
 }
---
+++ src/components/Search.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Search = () => {
+  return (
+    <div className='container mx-auto bg-[#646262] w-[60rem] h-[40rem] rounded-xl shadow-2xl' >
+        
+    </div>
+  )
+}
+
+export default Search
