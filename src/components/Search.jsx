8d03a23661e4034ff9b1cd92927be1c30cbcb985
jayaadeep11 <jayadeep.bellamkonda@gmail.com>
2024-05-18 13:17:42 +0530

    added search-input component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import UserSearch from './search-input'
 
 const Search = () => {
   return (
-    <div className='container mx-auto bg-[#646262] w-[60rem] h-[40rem] rounded-xl shadow-2xl' >
-        
+    <div className='container mx-auto bg-black flex justify-center  w-[60rem] h-[40rem] rounded-xl shadow-2xl' >
+      <UserSearch />
     </div>
   )
 }
