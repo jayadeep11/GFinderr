4aecf9ec825a88146da6346770c4f6cc4b25b9dc
uyyalanaveen <21131a1254@gvpce.ac.in>
2024-06-24 23:57:35 +0530

    updated-userdetailspage

--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -50,12 +50,12 @@ const UserDetails = () => {
       <div className='mx-auto container h-full bg-white'>
         <img src={avatar_url} alt={name} className='rounded-full w-32 h-32 mb-4' />
         <h1 className='text-xl font-bold'>{name}</h1>
-        {/* <p className='text-gray-700'>{bio || 'No bio provided'}</p>
-        <p className='text-gray-500'>Username: {login}</p> */}
+        <p className='text-gray-700'>{bio || 'No bio provided'}</p>
+        <p className='text-gray-500'>Username: {login}</p>
         <button className='border hover:bg-black  text-center hover:text-white  border-gray-500 text-black font-normal  p-3  rounded-full'>
           <a href={html_url} className='text-[1rem]'>View in Github</a>
         </button>
-        
+
 
       </div>
     </>
